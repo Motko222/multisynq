07211e4f48d639f96738752ac391856fd56ec466
Motko <77466238+Motko222@users.noreply.github.com>
2025-06-15 22:43:06 +0200

    Update install-service.sh

--- install-service.sh
+++ install-service.sh
@@ -4,24 +4,17 @@ source $path/env
 
 #create service
 printf "[Unit]
-Description=$folder node
-After=network.target
-Wants=network-online.target
+Description=Multisynq Synchronizer headless service
+After=docker.service
+Requires=docker.service
 
 [Service]
-EnvironmentFile=/root/scripts/$folder/env
-Environment=""
+Type=simple
 User=root
-Group=root
-ExecStart=$folder
 Restart=always
-RestartSec=30
-LimitNOFILE=65536
-LimitNPROC=4096
-StandardOutput=journal
-StandardError=journal
-SyslogIdentifier=$folder
-WorkingDirectory=/root/$folder
+RestartSec=10
+ExecStart=/usr/bin/docker run --rm --name synchronizer-cli --pull always --platform linux/amd64 cdrakep/synqchronizer:latest --depin wss://api.multisynq.io/depin --sync-name $NAME --launcher $LAUNCHER --key $KEY --wallet $WALLET
+Environment=PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 
 [Install]
 WantedBy=multi-user.target" > /etc/systemd/system/$folder.service
